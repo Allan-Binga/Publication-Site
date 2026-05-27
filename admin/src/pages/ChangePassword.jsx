5664d4cf81b43a731bb39286e91ea57391781a7b
Allan-Binga <allanbinga73@gmail.com>
2026-05-27 10:07:44 -0400

    Password Change Fix

--- admin/src/pages/ChangePassword.jsx
+++ admin/src/pages/ChangePassword.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/Navbar"
+import LoginNav from "../components/LoginNav"
 import Footer from "../components/Footer"
 import api from "../interceptor"
 import Spinner from "../components/Spinner"
@@ -137,7 +137,7 @@ function ChangePassword() {
 
     return (
         <div className="min-h-screen flex flex-col">
-            <Navbar />
+            <LoginNav />
 
             <main className="flex-grow flex items-center justify-center p-4">
                 <div className="w-full max-w-md bg-white rounded-lg border border-slate-300 overflow-hidden">
